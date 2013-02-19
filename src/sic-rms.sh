0389653c160324e4c6fce10d8d6be6ea74f7e3e0
Paulo Matos <paulo@matos-sorge.com>
2013-02-19 22:08:23 +0000

    Initial version of the rms command.

---
+++ src/sic-rms.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# Remove photo from gallery given its sha1,
+# introduce sha1 into the removed files list
+
+source common.sh
+
+REMFILE=$SICHOME/removed
+SHA=$1
+
+if [ ! -d $DATADIR/$SHA ]; then
+    echo "File $SHA doesn't exist in gallery" >&2
+    exit 1
+fi
+
+# Remove file
+rm -Rf $DATADIR/$SHA
+
+# Add removed SHA to removed file
+touch $REMFILE # do i need to do this if doesn't exist?
+echo $SHA >> $REMFILE
+
+echo "File $SHA removed successfully"
+
+
+
