00f06b7c3c5c82b45d23647bd220d868f199e835
Paulo Matos <pmatos@broadcom.com>
2013-02-21 14:44:15 +0000

    Use writeppm application instead of scripting to generate random ppms.

--- scripts/rndimg.sh
+++ scripts/rndimg.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-source random-between.sh
-
 # Generate a random image
 
 WIDTH=${1}
@@ -11,21 +9,7 @@ OUTPUT=${3}
 PPM=/tmp/rndimg-${RANDOM}.ppm
 
 # Create ppm
-echo "P3" > $PPM
-echo $WIDTH >> $PPM
-echo $HEIGHT >> $PPM
-echo 255 >> $PPM
-
-for row in $(seq 1 $HEIGHT); do
-    for col in $(seq 1 $WIDTH); do
-	for colour in $(seq 1 3); do
-	    randomBetween 0 255 1
-	    echo -n $randomBetweenAnswer " " >> $PPM
-	done
-	echo -n " " >> $PPM
-    done
-    echo >> $PPM
-done
+./writeppm $WIDTH $HEIGHT $PPM
 
 # Convert to output
 convert $PPM $OUTPUT
