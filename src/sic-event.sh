abf25a21d02aad18f6bdc4c94d5a94947a0e752b
Paulo Matos <paulo@matos-sorge.com>
2013-02-25 21:59:02 +0000

    Commit initial version of events command : does nothing yet!

---
+++ src/sic-event.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+#
+# Event management
+# Events are collections of photos,
+# usually spanning a sequence of dates: a weekend,
+# a week, two weeks even...
+#
+# This is just a bunch of files (named after the event name)
+# which contain itself a list of photos/videos.
+#
+
+source common.sh
+
+HELP=0
+VERBOSE=""
+DEL=0
+
+# Available commands
+# new: create new event
+# add: add photo to event
+# rm: delete photo from event
+# -d: delete event
+
+while getopts ":hvd" opt; do
+    case $opt in
+	h)
+	    HELP=1
+	    ;;
+	v)
+	    VERBOSE="-v"
+	    ;;
+	d)
+	    DEL=1
+	    ;;
+	\?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    exit 1
+	    ;;
+    esac
+done
+shift $(( $OPTIND - 1))
+
+if (( $HELP == 1 )); then
+    echo "HELP"
+fi
+
