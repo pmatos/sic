189ba0d9ee0abd5a89c93549092bb44b7db74803
Paulo Matos <paulo@matos-sorge.com>
2013-03-02 13:33:35 +0000

    Finish implementation of sic event command.

--- src/sic-event.sh
+++ src/sic-event.sh
@@ -16,24 +16,25 @@ HELP=0
 VERBOSE=""
 DEL=0
 
+mkdir -p $SICHOME/events
+
 # Given an event name it returns the path to the event directory
 event_path ()
 {
     EVNAME=$1
-    return $SICHOME/events/$1
+    echo $SICHOME/events/$EVNAME
 }
 
 event_exists_p ()
 {
     EVNAME=$1
     
-    event_path $EVNAME
-    EVPATH=$?
+    EVPATH=$(event_path $EVNAME)
 
     if [ -d $EVPATH ]; then
-	return 0
-    else
 	return 1
+    else
+	return 0
     fi
 }
 
@@ -43,13 +44,50 @@ event_new ()
 {
     EVNAME=$1
     
-    if (( $(event_exists_p $EVNAME) == 1 )); then
+    if event_exists_p $EVNAME; then
 	echo "Event $EVNAME already exists"
 	exit 1
     fi
     
     touch $(event_path $EVNAME)
     echo "Event $EVNAME created"
+    exit 0
+}
+
+# Add single file to an event
+event_ads ()
+{
+    EVNAME=$1
+    SHA=$2
+
+    # First we should check if the file
+    # is already part of the event.
+    if grep -q $SHA $(event_path $EVNAME); then
+	echo "File $SHA is already part of event $EVNAME"
+	exit 1
+    fi
+
+    # Lets add it
+    echo $SHA >> $(event_path $EVNAME)
+    echo "File $SHA added successfully to event $EVNAME"
+    exit 0
+}
+
+# Remove single file from an event
+event_rms ()
+{
+    EVNAME=$1
+    SHA=$2
+    
+    # Check if file is not part of event
+    if ! grep -q $SHA $(event_path $EVNAME); then
+	echo "File $SHA is not part of event $EVNAME"
+	exit 1
+    fi
+    
+    # Lets remove it
+    sed -i "/$SHA/d" $(event_path $EVNAME)
+    echo "File $SHA removed successfully from event $EVNAME"
     exit 0
 }
 
@@ -82,10 +120,10 @@ if (( $HELP == 1 )); then
     echo "HELP"
 fi
 
-if (( $DEL == 1)); then
+if (( $DEL == 1 )); then
     EVNAME=$1
     
-    if (( !$(event_exists_p $EVNAME) )); then
+    if ! event_exists_p $EVNAME; then
 	echo "Event $EVNAME does not exist"
 	exit 1
     fi
@@ -97,11 +135,11 @@ fi
 
 COMMAND=$1
 
-if (( $COMMAND == "new" )); then
+if [ $COMMAND == "new" ]; then
     event_new $2
-elif (( $COMMAND == "ads" )); then
+elif [ $COMMAND == "ads" ]; then
     event_ads $2 $3
-elif (( $COMMAND == "rms" )); then
+elif [ $COMMAND == "rms" ]; then
     event_rms $2 $3
 else
     echo "Unknown command: $COMMAND"
